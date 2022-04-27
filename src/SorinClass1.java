92fad21a9584d0e302a9ed20bc39c2896fef53bb
Cemal Polat <cemalpolat0001@gmail.com>
2022-04-27 23:18:44 +0100

    Cemal1 created

--- src/SorinClass1.java
+++ src/SorinClass1.java
@@ -1,7 +1,14 @@
 public class SorinClass1 {
     public static void main(String[] args) {
         System.out.println("Hello Cydeo");
+
+
+        System.out.println("Cemal addition");
+
+
     }
+
+
 
 
 
